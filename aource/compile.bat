3f5d40b3d420cf803bf1677c6a784c7128d2d8cd
Adam Jimenez <adam.jimenez@gmail.com>
2015-01-05 20:30:23 +0000

    latest build

--- aource/compile.bat
+++ aource/compile.bat
@@ -1,3 +1,3 @@
 taskkill /im XBoxMouse.exe
 del XBoxMouse.exe
-"%programfiles(x86)%\AutoHotkey\Compiler\Ahk2exe.exe" /in "XBoxMouse.ahk" /icon "controller.ico" /pass "CustomPassword" /NoDecompile
+"%programfiles(x86)%\AutoHotkey\Compiler\Ahk2exe.exe" /in "XBoxMouse.ahk" /icon "XBoxMouse.ico" /pass "CustomPassword" /NoDecompile
