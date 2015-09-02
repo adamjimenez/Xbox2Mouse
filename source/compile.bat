bc1c9a5f99193f1341ea7167f3a161d6136a068d
Adam Jimenez <adam.jimenez@gmail.com>
2015-09-02 19:01:33 +0100

    update build

--- source/compile.bat
+++ source/compile.bat
@@ -1,4 +1,4 @@
 taskkill /im Xbox2Mouse.exe
 del ../Xbox2Mouse.exe
-"%programfiles(x86)%\AutoHotkey\Compiler\Ahk2exe.exe" /in "Xbox2Mouse.ahk" /icon "Xbox2Mouse.ico"
+"%programfiles%\AutoHotkey\Compiler\Ahk2exe.exe" /in "Xbox2Mouse.ahk" /icon "Xbox2Mouse.ico"
 move Xbox2Mouse.exe ../
